2c4aa444ea0b0656c50f1f5667c52434653e3ff7
badpingpong <akirayo.badpingpong@gmail.com>
2020-07-14 16:03:49 +0900

    tweet button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Hello%20world" data-size="large">Tweet></a>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
